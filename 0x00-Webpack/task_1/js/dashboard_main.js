31430699d5296d3109a8928433fad55d4ffe5fd9
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-01-24 02:42:36 +0300

    Fix code

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,21 @@
 import $ from "jquery";
 import _ from "lodash";
 
-$("p").add("<p>Holberton Dashboard</p>").appendTo(document.body);
-$("p").add("<p>Dashboard data for the students</p>").appendTo(document.body);
-$("p:nth-child(2)")
-  .add("<button>Click here to get started</button>")
-  .appendTo(document.body);
-$("p").add("<p id='count'></p>").appendTo(document.body);
-$("p").add("<p>Copyright - Holberton School</p>").appendTo(document.body);
+$.when($.ready).then(function () {
+  $("p").add("<p>Holberton Dashboard</p>").appendTo(document.body);
+  $("p").add("<p>Dashboard data for the students</p>").appendTo(document.body);
+  $("p:nth-child(2)")
+    .add("<button>Click here to get started</button>")
+    .appendTo(document.body);
+  $("p").add("<p id='count'></p>").appendTo(document.body);
+  $("p").add("<p>Copyright - Holberton School</p>").appendTo(document.body);
 
-// prevent spammers
-let count = 0;
-const updateCounter = () => {
-  count += 1;
-  $("#count").append(`${count} clicks on the button`);
-};
+  // prevent spammers
+  let count = 0;
+  const updateCounter = () => {
+    count += 1;
+    $("#count").append(`${count} clicks on the button`);
+  };
 
-$("button").on("click", _.debounce(updateCounter()));
+  $("button").on("click", _.debounce(updateCounter, 500));
+});
--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -2,7 +2,8 @@ const path = require("path");
 module.exports = {
   entry: "./js/dashboard_main.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
   },
+  mode: 'production'
 };
