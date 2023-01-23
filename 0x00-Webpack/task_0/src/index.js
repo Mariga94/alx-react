f7d0a2ee69c5e652cfca863403de02ef9e53167d
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-01-23 23:00:21 +0300

    Basic webpack setup

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+import $ from "jquery";
+
+$("document").add("<p>Holberton Dashboard</p>").appendTo(document.body);
+$("document")
+  .add("<p>Dashboard data for the students</p>")
+  .appendTo(document.body);
+$("document")
+  .add("<p>Copyright - Holberton School</p>")
+  .appendTo(document.body);
