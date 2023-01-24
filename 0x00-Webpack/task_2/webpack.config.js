ba5e4383523ff7a97e99893171d5189508ca91e0
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-01-24 14:42:47 +0300

    Improve webpack

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,44 +1,33 @@
-const path = require("path");
+const path = require('path');
+
 module.exports = {
-  entry: "./js/dashboard_main.js",
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-            },
-          },
-          {
-            loader: "style-loader",
-          },
-        ],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true,
-              disable: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
-  mode: "production",
-  performance: {
+	entry: './js/dashboard_main.js',
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	mode: 'production',
+	performance: {
     maxAssetSize: 100000,
   },
-};
+	module: {
+    rules: [
+			{
+				test: /\.css$/, use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/i,
+				use: [
+					'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true, // webpack@1.x
+							disable: true, // webpack@2.x and newer
+						},
+					},
+				],
+			}
+    ],
+	},
+}
