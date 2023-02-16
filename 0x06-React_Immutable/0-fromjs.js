5000788671f5979b89618ca63fd739f439195a97
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-02-17 02:07:47 +0300

    Add space at the end

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,7 +1,5 @@
 import { fromJS } from './node_modules/immutable/dist/immutable';
 
-const { Map } = require('immutable');
-
 export default function getImmutableObject(object) {
     return fromJS(object)
 }
