86a629cda050f71951e33514f4b895b0e2c14f26
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-02-18 04:20:23 +0300

    Completed

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,5 +1,6 @@
+import immutable from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-    for (const key in array) {
-        return object[key].first
-    }
+  const deepData = immutable.fromJS(object);
+  return deepData.getIn(array, undefined);
 }
