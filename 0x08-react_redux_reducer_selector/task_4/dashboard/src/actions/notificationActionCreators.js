7d9feba0db260a329ec6c5f8db3bb86fefdff3d1
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-03-02 02:00:44 +0300

    Normalizr & immutable

---
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,48 @@
+import {
+  MARK_AS_READ,
+  SET_TYPE_FILTER,
+  FETCH_NOTIFICATIONS_SUCCESS,
+} from "./notificationActionTypes";
+
+export const markAsRead = (index) => {
+  return {
+    type: MARK_AS_READ,
+    index: index,
+  };
+};
+
+export const setNotificationFilter = (filter) => {
+  return {
+    type: SET_TYPE_FILTER,
+    filter: filter,
+  };
+};
+
+export const fetchNotificationsSucess = () => {
+  return {
+    type: FETCH_NOTIFICATIONS_SUCCESS,
+    data: [
+      {
+        id: 1,
+        type: "default",
+        value: "New course available",
+      },
+      {
+        id: 2,
+        type: "urgent",
+        value: "New resume available",
+      },
+      {
+        id: 3,
+        type: "urgent",
+        value: "New data available",
+      },
+    ],
+  };
+};
+
+export const boundMarkAsRead = (index) => dispatch(markAsRead(index));
+export const boundesetNotificationFilter = (index) =>
+  dispatch(setNotificationFilter(index));
+export const boundNotificationsSucess = () =>
+  dispatch(fetchNotificationsSucess());
