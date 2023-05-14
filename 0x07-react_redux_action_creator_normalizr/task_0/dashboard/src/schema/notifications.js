b04a133b5c0e2953f759475a612ed9ef8dc67c70
Joseph Mariga <56932629+Mariga94@users.noreply.github.com>
2023-05-14 23:10:26 +0300

    Delete 0x07-react_redux_action_creator_normalizr directory

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++
@@ -1,7 +0,0 @@
-import * as data from "../../notifications.json";
-
-export function getAllNotificationsByUser(userId) {
-  return data.default
-    .filter((item) => item.author.id === userId)
-    .map(({ context }) => context);
-}
--- 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionTypes.js
+++
@@ -1,2 +0,0 @@
-export const MARK_AS_READ = 'MARK_AS_READ';
-export const SET_TYPE_FILTER = 'SET_TYPE_FITLER';
