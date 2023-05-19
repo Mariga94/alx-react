59258208bbbd1609a008d9a3f67e7da2d6d2c142
Mariga94 <56932629+Mariga94@users.noreply.github.com>
2023-05-19 04:06:22 +0300

    create actions for the course list

---
+++ 0x07-react_redux_action_creator_normalizr/task_3/dashboard/src/HOC/WithLogging.test.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { shallow, configure, mount } from "enzyme";
+import Adapter from "@cfaester/enzyme-adapter-react-18";
+import WithLogging from "./WithLogging";
+import Login from '../Login/Login.js';
+import { spy } from 'sinon';
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+configure({ adapter: new Adapter() });
+import { StyleSheetTestUtils } from "aphrodite";
+
+chai.use(sinonChai);
+let logs = spy(console, 'log');
+
+describe("test <WithLogging /> component", () => {
+
+	beforeEach(() => {
+		StyleSheetTestUtils.suppressStyleInjection();
+	  });
+	  afterEach(() => {
+		StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+	  });
+    test("console.log was called on mount and on unmount with Component when the wrapped element is pure html", () => {
+		let wrapper = mount(
+			<WithLogging>
+				<p>simple phrase</p>
+			</WithLogging>
+		);
+		expect(logs).to.have.been.calledWith('Component Component is mounted');
+		wrapper.unmount();
+		expect(logs).to.have.been.calledWith('Component Component is going to unmount');
+	});
+
+	test("Renders the correct children with <Login /> Component as a child", () => {
+		let wrapper = mount(
+			<WithLogging>
+				<Login />
+			</WithLogging>
+      );
+		expect(logs).to.have.been.calledWith('Component Login is mounted');
+		wrapper.unmount();
+		expect(logs).to.have.been.calledWith('Component Login is going to unmount');
+	});
+})
